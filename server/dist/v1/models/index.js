47f3193de1e0f7803f20d57bbc40d54c2a00c392
hamzagh1998 <bmca.1920@gmail.com>
2022-08-29 17:38:59 +0100

    add /dist to .gitignore

--- server/dist/v1/models/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ChatModel = exports.MessageModel = exports.UserModel = void 0;
-var user_model_1 = require("./user.model");
-Object.defineProperty(exports, "UserModel", { enumerable: true, get: function () { return user_model_1.UserModel; } });
-var message_model_1 = require("./message.model");
-Object.defineProperty(exports, "MessageModel", { enumerable: true, get: function () { return message_model_1.MessageModel; } });
-var chat_model_1 = require("./chat.model");
-Object.defineProperty(exports, "ChatModel", { enumerable: true, get: function () { return chat_model_1.ChatModel; } });
--- server/dist/v1/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.spawnWorker = exports.getWorkerIdx = void 0;
-var getWorkerIdx_1 = require("./getWorkerIdx");
-Object.defineProperty(exports, "getWorkerIdx", { enumerable: true, get: function () { return getWorkerIdx_1.getWorkerIdx; } });
-var spawnWorker_1 = require("./spawnWorker");
-Object.defineProperty(exports, "spawnWorker", { enumerable: true, get: function () { return spawnWorker_1.spawnWorker; } });
